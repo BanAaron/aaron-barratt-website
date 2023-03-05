07f878c17fa2e090aca31cb22e0e630fb6c7de76
Aaron Barratt <aarontbarratt@gmail.com>
2023-03-05 14:47:16 +0000

    Getting there! Links work and layout getting shaped up

--- views.py
+++ views.py
@@ -3,12 +3,24 @@ from flask import Blueprint, render_template, redirect, url_for
 views = Blueprint(__name__, "views")
 
 
-@views.route("/")
-def home_page():
-    return render_template("index.html", name="Aaron")
+@views.route("/about")
+def about_page():
+    return render_template(
+        "index.html", name="Aaron Barratt.", tag_line="I build things in computers."
+    )
+
+
+@views.route("/projects")
+def projects_page():
+    return render_template("projects.html")
+
+
+@views.route("/work")
+def work_page():
+    return render_template("work.html")
 
 
 # this is how to redirect
-@views.route("go-home")
+@views.route("/")
 def go_home():
-    return redirect(url_for("views.home"))
+    return redirect(url_for("views.about_page"))
