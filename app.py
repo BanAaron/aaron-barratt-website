ad0a7eca3e88b2eea1b0de15a14385cfc529005d
Aaron Barratt <aarontbarratt@gmail.com>
2023-03-15 00:18:02 +0000

    Fixed resume section padding

--- app.py
+++ app.py
@@ -8,5 +8,3 @@ if __name__ == "__main__":
     app.run(debug=True, port=8000)
 
 # TODO: fix about-me images on desktop
-# TODO: 10. General: Probably want to add contact details (mobile/email) next to your socials
-# TODO: 11. Projects - Might be worth putting in bigger descriptions - talk about why you wanted to do the project, what you used, what you learnt - that kind of thing. Maybe put in a few images here as well
