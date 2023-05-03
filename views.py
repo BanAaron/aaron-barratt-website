9e805d905b1801d7a636744a309fece0a527a2ff
Aaron Barratt <aarontbarratt@gmail.com>
2023-05-03 17:28:40 +0100

    ❌ removed unneeded routes

--- views.py
+++ views.py
@@ -8,23 +8,3 @@ def home_page():
     return render_template(
         "index.html", name="Aaron Barratt", tag_line="Python/SQL Developer"
     )
-
-
-@views.route("/about")
-def about_page():
-    return render_template("about.html")
-
-
-@views.route("/projects")
-def projects_page():
-    return render_template("projects.html")
-
-
-@views.route("/work")
-def work_page():
-    return render_template("work.html")
-
-
-@views.route("/test")
-def test_page():
-    return render_template("test.html")
