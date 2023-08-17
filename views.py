269c04f4519a31b81f1bd5805c8a0998dfd74a4b
Aaron Barratt <aarontbarratt@gmail.com>
2023-08-17 19:51:23 +0100

    added dentsu to roles section

--- views.py
+++ views.py
@@ -6,5 +6,5 @@ views = Blueprint(__name__, "views")
 @views.route("/")
 def home_page():
     return render_template(
-        "index.html", name="Aaron Barratt", tag_line="Python/SQL Developer"
+        "index.html", name="Aaron Barratt", tag_line="Full Stack Developer"
     )
